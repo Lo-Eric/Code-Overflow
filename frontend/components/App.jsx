d3cb128cdb55ef3489c05ceb51cff88a770f400b
loeric33@gmail.com <loeric33@gmail.com>
2020-06-02 16:17:33 -0400

    completed signup/login/logout functionality

---
+++ frontend/components/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Provider } from 'react-redux';
+import {
+    Route,
+    Redirect,
+    Switch,
+    Link,
+    HashRouter
+} from 'react-router-dom';
+
+import GreetingContainer from "./greeting/greeting_container";
+import SignupFormContainer from './session_form/signup_form_container';
+import LoginFormContainer from './session_form/login_form_container';
+
+const App = () => (
+    <div>
+        <header>
+            <h1>Code Overflow from App</h1>
+            <GreetingContainer />
+        </header>
+
+        <Route path="/login" component={LoginFormContainer} />
+        <Route path="/signup" component={SignupFormContainer} />
+    </div>
+);
+
+export default App;
