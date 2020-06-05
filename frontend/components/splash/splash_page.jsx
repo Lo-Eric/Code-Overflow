273ddfed75c3532262926727f6dd4a0517a46bc7
loeric33@gmail.com <loeric33@gmail.com>
2020-06-05 08:46:49 -0400

    more splash styling

--- frontend/components/splash/splash_page.jsx
+++ frontend/components/splash/splash_page.jsx
@@ -11,17 +11,28 @@ const Splash = () => {
             <section className="content2-1">
                 <h1>For developers, by developers</h1>
                 <div>bar-pill</div>
-                <p>Stack Overflow is an open community for anyone that codes. We help you get answers to your toughest coding questions, share knowledge with your coworkers in private, and find your next dream job.</p>
+                <p>Code Overflow is an open community for anyone that codes. We help you get answers to your toughest coding questions, share knowledge with your coworkers in private, and find your next dream job.</p>
             </section>
             <section className="content2-2">
                 <div id="box1">
                     <img src="https://cdn.sstatic.net/Img/home/public-qa.svg?v=d82acaa7df9f" alt=""/>
                     <h1>Public Q &#38; A</h1>
                     <p>Get answers to more than 16.5 million questions and give back by sharing your knowledge with others. Sign up for an account.</p>
-                    <button className="button">Browse questions</button>
+                    <button className="button1">Browse questions</button>
                 </div>
-                <div id="box2">Private Q &#38; A</div>
-                <div id="box3">Browse Jobs</div>  
+                <div id="box2">
+                    <img src="https://cdn.sstatic.net/Img/home/private-qa.svg?v=2c1de180b6d7" alt="" />
+                    <h1>Private Q &#38; A</h1>
+                    <p>Level up with Code Overflow while you work. Share knowledge privately with your coworkers using our flagship Q &#38; A engine.</p>
+                    <button className="button2">Try for free</button>
+                </div>
+                <div id="box3">
+                    <img src="https://cdn.sstatic.net/Img/home/private-qa.svg?v=2c1de180b6d7" alt="" />
+                    <h1>Browse jobs</h1>
+                    <p>Find the right job through high quality listings and search for roles based on title, technology stack, salary, location, and more.</p>
+                    <button className="button3">Find a job</button>
+                </div> 
+                    
             </section>     
     </div>    
     )
