560fc616c4474d2f340053785b5c76645b6c0f41
loeric33@gmail.com <loeric33@gmail.com>
2020-06-12 06:32:17 -0400

    more styling

---
+++ frontend/components/questions/create_question.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from "react-redux";
+import Footer from '../splash/footer'
+import LeftNavBar from '../left_nav_bar/left_nav'
+
+class CreateQuestionForm extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="create-question-form">
+                <section className="left-nav-bar"><LeftNavBar/></section>
+            </div>
+        )
+    }
+}
+
+export default CreateQuestionForm;
