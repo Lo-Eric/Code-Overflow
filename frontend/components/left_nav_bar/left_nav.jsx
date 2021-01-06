f2ac9e2a3adcf5bebe6aa2ea3e302689a1d518e4
loeric33@gmail.com <loeric33@gmail.com>
2021-01-06 01:43:22 -0500

    styling

--- frontend/components/left_nav_bar/left_nav.jsx
+++ frontend/components/left_nav_bar/left_nav.jsx
@@ -7,11 +7,18 @@ const LeftNavBar = () => (
 
         <li className="public">Public</li>
 
-        <li><Link to="/home">
-          <img id='globe' src="assets/globe.png" height='19' width='19' alt=""/>
+        <div className='public-content'>
+        <li className="globe-link"><Link to="/home">
+          <img id='globe-img' src="assets/globe.png" height='19' width='19' alt=""/>
           Code Overflow
           {/* <li>Code Overflow</li> */}
         </Link></li>
+
+        <li className="tags">Tags</li>
+
+        <li className="users">Users</li>
+        </div>
+
     </div>
 )
 
